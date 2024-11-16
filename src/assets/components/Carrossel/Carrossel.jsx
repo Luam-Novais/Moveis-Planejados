dce7cc6677db6d9341f3ddf2c02dd41a2559278a
Luam-Novais <luampabl014@gmail.com>
2024-11-16 14:19:34 -0300

    Meu segundo commit

--- src/assets/components/Carrossel/Carrossel.jsx
+++ src/assets/components/Carrossel/Carrossel.jsx
@@ -2,18 +2,40 @@ import React, { useState } from 'react';
 import styles from './Carrossel.module.css';
 import { GrPrevious, GrNext } from 'react-icons/gr';
 
+const imgs = ['sala', 'quarto', 'cozinha'];
+
 export const Carrossel = () => {
-  const [carrossel, setCarrossel] = useState([]);
+  const [index, setIndex] = useState(2);
+
+  const handlePrevImg = () => {
+    // if(index <= 2){
+    //     setIndex(prevIndex => prevIndex - 1)
+    // }
+    // else if(index === 2){
+    //     setIndex(0)
+    // }
+    console.log(index);
+  };
+  const handleNextImg = () => {
+    // if(index <= 2){
+    //     setIndex(prevIndex => prevIndex + 1)
+    // }
+    // else if(index === 2){
+    //     setIndex(0)
+    // }
+    console.log(index);
+  };
+
   return (
     <section className={styles.Container}>
       <h2>Forma que acompanha a vida.</h2>
-      <div className={styles.carrosselContainer} style={{ backgroundImage: "url('./public/assets/carrossel/cozinha-carrossel.jpg')" }}>
-        <button className={styles.btnPrev}>
+      <div className={styles.carrosselContainer} style={{ backgroundImage: `url('./public/assets/carrossel/${imgs[index]}-carrossel.jpg')` }}>
+        <button onClick={handlePrevImg} className={styles.btnPrev}>
           <i>
-            <GrPrevious style={{backgroundColor: '#fff !important'}}/>
+            <GrPrevious style={{ backgroundColor: '#fff !important' }} />
           </i>
         </button>
-        <button className={styles.btnNext}>
+        <button onClick={handleNextImg} className={styles.btnNext}>
           <i>
             <GrNext />
           </i>
