b276bf811ac266362002dd396d6ee6ec8a76d620
Luam-Novais <luampabl014@gmail.com>
2025-02-20 23:14:58 -0300

    fix: update image paths for consistency across components

--- src/database.js
+++ src/database.js
@@ -2,16 +2,16 @@ export const slides = [
     {
         tittle: 'Cozinha planejada.',
         paragraph:'Muito além de uma área para cozinhar, a cozinha é um dos pontos-chave da casa: é nela que acontecem os melhores encontros! Opções planejadas são ideais para aproveitar melhor os espaços e deixar o ambiente aconchegante e com a sua cara. ',
-        img: './public/carrossel/cozinha-carrossel.jpg '
+        img: '/carrossel/cozinha-carrossel.jpg'
     },
     {
         tittle: 'Sala de Estar.',
         paragraph:'Muito mais do que um lugar para relaxar, a sala de estar é o coração da casa: é onde os momentos mais especiais ganham vida! Soluções planejadas são perfeitas para otimizar espaços e criar um ambiente acolhedor, elegante e com a sua personalidade em cada detalhe.',
-        img: './public/carrossel/sala-carrossel.jpg '
+        img: '/carrossel/sala-carrossel.jpg'
     },
     {
         tittle: 'Quarto e closet.',
         paragraph:'Aproveite melhor cada espaço do seu quarto e closet, trazendo beleza, praticidade e sofisticação aos ambientes. Aqui, você encontra tudo para fazer seu quarto planejado, da criação à montagem, com os mesmos acabamentos e qualidade nos móveis soltos e planejados!',
-        img: './public/carrossel/quarto-carrossel.jpg '
+        img: '/carrossel/quarto-carrossel.jpg'
     }
 ]
--- src/pages/GridImages.jsx
+++ src/pages/GridImages.jsx
@@ -8,15 +8,15 @@ const GridImages = ({galeria}) => {
       
         <h1>Explore ideias<span>.</span></h1>
         <div className={styles.gridContainer1}>
-            <img className={`${styles.img} ${styles.imgDeitada} ${styles.img1}`} src="./public/grid/img-grid1.jpg" alt="" />
-            <img className={`${styles.img} ${styles.imgDeitada} ${styles.img2}`} src="./public/grid/img-grid2.jpg" alt="" />
-            <img className={`${styles.img} ${styles.imgEmPe} ${styles.img3}`} src="./public/grid/img-grid3.jpg" alt="" />
+            <img className={`${styles.img} ${styles.imgDeitada} ${styles.img1}`} src="/grid/img-grid1.jpg" alt="" />
+            <img className={`${styles.img} ${styles.imgDeitada} ${styles.img2}`} src="/grid/img-grid2.jpg" alt="" />
+            <img className={`${styles.img} ${styles.imgEmPe} ${styles.img3}`} src="/grid/img-grid3.jpg" alt="" />
         </div>
         <div className={styles.gridContainer2}>
-            <img className={`${styles.img} ${styles.imgEmPe} ${styles.img4}`} src="./public/grid/img-grid4.jpg" alt="" />
-            <img className={`${styles.img} ${styles.imgQua} ${styles.img5}`} src="./public/grid/img-grid5.jpg" alt="" />
-            <img className={`${styles.img} ${styles.imgQua} ${styles.img6}`} src="./public/grid/img-grid6.jpg" alt="" />
-            <img className={`${styles.img} ${styles.imgDeitada} ${styles.img7}`} src="./public/grid/img-grid7.jpg" alt="" />
+            <img className={`${styles.img} ${styles.imgEmPe} ${styles.img4}`} src="/grid/img-grid4.jpg" alt="" />
+            <img className={`${styles.img} ${styles.imgQua} ${styles.img5}`} src="/grid/img-grid5.jpg" alt="" />
+            <img className={`${styles.img} ${styles.imgQua} ${styles.img6}`} src="/grid/img-grid6.jpg" alt="" />
+            <img className={`${styles.img} ${styles.imgDeitada} ${styles.img7}`} src="/grid/img-grid7.jpg" alt="" />
         </div>
         <Button/>
     </section>
